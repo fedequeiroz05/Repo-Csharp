ed77557c05490bfb73a4552ba9ae62f8f69f2611
Fededaher <felipequeiroz05@gmail.com>
2024-02-19 19:24:20 -0300

    Esse commit estou adicionando o projeto 2 criando variavel

---
+++ ProjetoCSharp/CriandovariavelCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandovariavelCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo, criando variavel projeto Csharp2");
+            Console.WriteLine("Projeto 2 finalizar, pressione enter para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
