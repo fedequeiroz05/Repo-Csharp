22a492045a5034dfeb6b035f6fba9ff65d5fcf13
Fededaher <felipequeiroz05@gmail.com>
2024-02-21 22:05:50 -0300

    atualização dia 21/02/2024 - Aula criando variavel double

--- ProjetoCSharp/CriandovariavelCsharp/Program.cs
+++ ProjetoCSharp/CriandovariavelCsharp/Program.cs
@@ -10,9 +10,27 @@ namespace CriandovariavelCsharp
     {
         static void Main(string[] args)
         {
+            int idade;
+            idade = 10;
+            Console.WriteLine("Sua idade é " + idade);
+
+            idade = (10 + 10) - 3;
+            Console.WriteLine("minha mãe tem " + idade);
+
+            double salario;
+            salario = 3200.45;
+
+            double tamanho;
+            tamanho = 3.0 / 2;
+            Console.WriteLine("Meu verdadeiro tamanho é " + tamanho);
+
+            Console.WriteLine("Eu recebo mensalmente R$" + salario);
+
             Console.WriteLine("Ola mundo, criando variavel projeto Csharp2");
             Console.WriteLine("Projeto 2 finalizar, pressione enter para fechar...");
+            Console.WriteLine(idade);
             Console.ReadLine();
+
         }
     }
 }
